be9006d7771ec02d3bc69d1c44c11d65f7e27332
gaeunnlee <gaeunnnleee@gmail.com>
2022-09-05 01:18:53 +0900

    feat: first commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Seo from "../components/Seo";
+
+export default function About() {
+    return (
+      <div>
+        <Seo title="About" />
+        <h1>About Us</h1>
+        <img src="/vercel.svg" />
+      </div>
+    )
+  }
+  
