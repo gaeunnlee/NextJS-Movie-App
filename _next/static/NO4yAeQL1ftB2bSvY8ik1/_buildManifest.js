1337aef3e7b5009d141a86ee73152a4b4a64eb20
gaeunnlee <gaeunnlee@users.noreply.github.com>
2022-09-11 14:57:34 +0000

    deploy: cb526b3cd3f26e0c9e11e43a2395889a54045cc3

--- _next/static/NO4yAeQL1ftB2bSvY8ik1/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-f1db6142a6ac9ddc.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],"/about":["static/chunks/pages/about-8102c9fe9c4bc9b4.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
