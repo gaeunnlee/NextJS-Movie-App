9e2c488f50b5d377c9926aa31dbc8d3ee6185dba
gaeunnlee <gaeunnlee@users.noreply.github.com>
2022-09-12 11:58:35 +0000

    deploy: 472dcf0da11341e16ff5e0dae101b935e688517f

--- _next/static/-36rS_SnOkixfcookvs2y/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/api/movies"}],fallback:[]},"/":["static/chunks/pages/index-a7c65b64747436f7.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],"/about":["static/chunks/pages/about-58d8a92699f9721b.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
