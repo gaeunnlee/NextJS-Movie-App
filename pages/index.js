f68305251b149c017c2778a25b1d35641fca9005
gaeunnlee <gaeunnnleee@gmail.com>
2022-09-14 23:45:56 +0900

    feat: Server Side Rendering

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,11 @@
 import { useEffect, useState } from "react"
 import Seo from "../components/Seo";
-const API_KEY = "a82ffdd3600a2812df822bf07033a3e0"
 
-export default function Home() {
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    (async() => {
-      const { results } = await(await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)).json();
-      setMovies(results);
-      console.log(results);
-    })()
-  },[])
+export default function Home({results}) {
   return (
     <div id="movie_content">
       <Seo title="Home" />
-      {movies.length <= 0 && <h4>Loading...</h4>}
-      {movies?.map(movie => <div className="movie" key={movie.id}>
+      {results?.map(movie => <div className="movie" key={movie.id}>
         <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}/>
         <h4>{movie.original_title}</h4>
         <p>{movie.overview}</p>
@@ -46,4 +36,14 @@ export default function Home() {
       `}</style>
     </div>
   )
+}
+
+export async function getServerSideProps(){
+  const API_KEY = "a82ffdd3600a2812df822bf07033a3e0"
+  const { results } = await(await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)).json();
+  return {
+    props: {
+      results
+    }
+  }
 }
